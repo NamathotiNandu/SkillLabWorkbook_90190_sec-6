78fedd9a004857dd194c712e788f751100e1ed36
NamathotiNandu <nandunamathoti1@gmail.com>
2026-02-12 14:15:57 +0530

    Added Experiment4 and Experiment5

---
+++ Experiment4/src/main/java/com/fsad/main/StudentAnnoMain.java
@@ -0,0 +1,17 @@
+package com.fsad.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.fsad.config.AppConfig;
+import com.fsad.model.Student;
+
+public class StudentAnnoMain {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Student student = context.getBean(Student.class);
+        student.display();
+    }
+}
